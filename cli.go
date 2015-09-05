5a072634156e1349d0dd41a01a890d7d3d698eee
daigo <dragonmind2002@yahoo.co.jp>
2015-09-05 21:00:13 +0900

    first commit

---
+++ cli.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+)
+
+// Exit codes are int values that represent an exit code for a particular error.
+const (
+	ExitCodeOK    int = 0
+	ExitCodeError int = 1 + iota
+)
+
+// CLI is the command line object
+type CLI struct {
+	// outStream and errStream are the stdout and stderr
+	// to write message from the CLI.
+	outStream, errStream io.Writer
+}
+
+// Run invokes the CLI with the given arguments.
+func (cli *CLI) Run(args []string) int {
+	var (
+		n bool
+	)
+
+	// Define option flag parse
+	flags := flag.NewFlagSet(Name, flag.ContinueOnError)
+	flags.SetOutput(cli.errStream)
+
+	flags.BoolVar(&n, "n", false, "show what would have been transferred")
+	flags.BoolVar(&n, "n", false, "show what would have been transferred(Short)")
+
+	flVersion := flags.Bool("version", false, "Print version information and quit.")
+
+	// Parse commandline flag
+	if err := flags.Parse(args[1:]); err != nil {
+		return ExitCodeError
+	}
+
+	// Show version
+	if *flVersion {
+		fmt.Fprintf(cli.errStream, "%s version %s\n", Name, Version)
+		return ExitCodeOK
+	}
+
+	_ = n
+
+	return ExitCodeOK
+}
